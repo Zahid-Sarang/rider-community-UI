90fb19e331b4cd7f640ac795c63843c55f70b1de
zahid-soft <zahidsarang95@gmail.com>
2023-12-28 10:24:31 +0530

    setup ant design and tanstackQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
+import { ConfigProvider } from "antd";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router";
+import "./index.css";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: "#1E293B",
+                        colorLinkActive: "#334154",
+                        colorBgLayout: "#10172A",
+                        colorTextSecondary: "#FFFFFFB3",
+                    },
+                }}
+            >
+                <RouterProvider router={router} />
+            </ConfigProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
 );
